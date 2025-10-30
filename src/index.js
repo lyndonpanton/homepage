285113f0bef324628b2bf9e95742a9a31f842866
Lyndon Panton <LyndonPanton@gmail.com>
2025-10-30 04:44:55 +0000

    Setup: Configure webpack development server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 // console.log("Webpack configured successfully");
 
-import crowImage from "./asset/image/crow.jpg";
-
 import "./css/styles.css";
-
-const image = document.createElement("img");
-image.classList.add("image", "image-3");
-image.alt = "A black crow";
-image.src = crowImage;
-
-document.getElementsByTagName("main")[0].appendChild(image);
